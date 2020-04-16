2e98d5de77db526b725cd167d83bd79b81a07e64
Antoine J <ajegou@groupe-beaumanoir.fr>
2020-04-16 14:20:28 +0200

    TP1Module5LesChats

---
+++ TP1Module5LesChats/Controllers/ChatController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP1Module5LesChats.Models;
+
+namespace TP1Module5LesChats.Controllers
+{
+    public class ChatController : Controller
+    {        
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            List<Chat> listeChats = Data.Instance.ListeChats;
+            return View(listeChats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            List<Chat> listeChats = Data.Instance.ListeChats;
+            Chat chat = listeChats.Where(c => c.Id == id).Single();
+            return View(chat);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            List<Chat> listeChats = Data.Instance.ListeChats;
+            Chat chat = listeChats.Where(c => c.Id == id).Single();
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                List<Chat> listeChats = Data.Instance.ListeChats;
+                Chat chatRemoving = listeChats.Where(c => c.Id == id).Single();
+                listeChats.Remove(chatRemoving);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TP1Module5LesChats/Models/Data.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP1Module5LesChats.Models
+{
+    public class Data
+    {
+        private static Data _instance;
+        
+        static readonly object instanceLock = new object();
+
+        private Data()
+        {
+            InitialisationData();
+        }
+
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null) 
+                {
+                    lock (instanceLock)
+                    {
+                        if (_instance == null) 
+                            _instance = new Data();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private List<Chat> listeChats= new List<Chat>();
+
+        public List<Chat> ListeChats
+        {
+            get { return listeChats; }
+            set { listeChats = value; }
+        }
+
+        private void InitialisationData()
+        {
+            listeChats = Chat.GetMeuteDeChats();
+        }
+    }
+}
